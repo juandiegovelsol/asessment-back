01eec3739dc6b544011f59a1c809757b73d70915
Juan <juandiegovelsol@gmail.com>
2023-05-04 17:59:42 -0500

    Cors fixed

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ import express from "express";
 import userRoutes from "./routes/user.routes.js";
 import listRoutes from "./routes/list.routes.js";
 import favsRoutes from "./routes/favs.routes.js";
+import cors from "cors";
 
 const PORT = process.env.PORT;
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello Assesment back-end API" });
